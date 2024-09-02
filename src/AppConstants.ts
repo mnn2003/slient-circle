ef236f2b3c6dc992aed479bb06f7b747c17271f1
Aman Tiwari <88995970+mnn2003@users.noreply.github.com>
2024-09-02 12:10:27 +0530

    Update AppConstants.ts

--- src/AppConstants.ts
+++ src/AppConstants.ts
@@ -1,7 +1,7 @@
 // Head over to the Credentials section in your app on CometChat Dashboard and where you can find your unique App ID, Region and Auth Key. 😊
 
 export const AppConstants = {
-  APP_ID: "APP_ID",
-  REGION: "REGION",
-  AUTH_KEY: "AUTH_KEY",
+  APP_ID: "263197fd252e92b0",
+  REGION: "in",
+  AUTH_KEY: "1b892a85f2c2c629b27a74418985e8d0c97c0db8",
 };
